59a4a13bb048c8cfd36cde7777e3a38612992e07
Amelia_Achrem <276022@student.pwr.edu.pl>
2025-12-01 15:55:13 +0100

    Dodanie klasy reprezentującej image, dodanie pliku image_class

---
+++ backend/cv/myapp/source/image_class.py
@@ -0,0 +1,19 @@
+class Image:
+
+    def __init__(self, image_id, file_path, size, value):
+  
+        self.image_id = image_id
+        self.file_path = file_path
+        self.size = size
+        self.value = value 
+
+    def receive(self):
+        pass
+
+
+    def process(self):
+        pass
+
+
+    def send(self):
+        pass
