a4a7d165e247309caa8774c4e927d289deee28fa
AdriannaWierzowiecka <wierzowiecka.adrianna@gmail.com>
2026-01-12 21:39:33 +0100

    Convert to .tiff and calculate length

--- backend/cv/myapp/urls.py
+++ backend/cv/myapp/urls.py
@@ -7,4 +7,5 @@ print("🔥 myapp.urls LOADED 🔥")
 urlpatterns = [
     path("import_slide/", views.import_slide, name="import_slide"),
     path("convert/", views.convert, name="convert"),
+    path("analyze/", views.analyze, name="analyze"),
 ]
