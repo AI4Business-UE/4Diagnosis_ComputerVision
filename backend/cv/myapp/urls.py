99c06e573945ed1bacfb2dda73226b983f503a90
AdriannaWierzowiecka <wierzowiecka.adrianna@gmail.com>
2026-03-17 20:08:32 +0100

    better-reading-backend

--- backend/cv/myapp/urls.py
+++ backend/cv/myapp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("convert/", views.convert, name="convert"),
     path("tiff/<str:job_id>/", views.get_tiff, name="get-tiff"),
     path("result-image/<str:image_name>/", views.get_result_image, name="get-result-image"),
-    path("fibrosis/", views.fibrosis, name="fibrosis"),
-    path("length/", views.length, name="length"),
+    path("fibrosis/", views.analyze_fibrosis_degree, name="analyze_fibrosis_degree"),
+    path("length/", views.measure_tissue_length, name="measure_tissue_length"),
     # path("glomerule/", views.analyze, name="glomerule"),
 ]
